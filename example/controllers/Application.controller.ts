7274e5f4d169168baea5ee3e31a13cf94d47d153
David De Anda <omarandstuff@gmail.com>
2023-09-28 17:55:53 -0600

    Remove example files

--- example/controllers/Application.controller.ts
+++
@@ -1,9 +0,0 @@
-import { BaseController, Controller, Get } from '@universal-packages/express-controllers'
-
-@Controller()
-export default class ApplicationController extends BaseController {
-  @Get()
-  public async test(): Promise<void> {
-    throw new Error('Bad')
-  }
-}
