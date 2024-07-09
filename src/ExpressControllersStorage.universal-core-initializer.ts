db93c0a00330191f386493e0fc5365d5b23e88eb
David De Anda <omarandstuff@gmail.com>
2024-07-08 23:32:48 -0600

    Initializer description

--- src/ExpressControllersStorage.universal-core-initializer.ts
+++ src/ExpressControllersStorage.universal-core-initializer.ts
@@ -2,6 +2,7 @@ import CoreInitializer from '@universal-packages/core/CoreInitializer'
 
 export default class ExpressControllersStorageInitializer extends CoreInitializer {
   public static readonly initializerName = 'express-controllers-storage'
+  public static readonly description: string = 'Express Controllers Storage initializer'
 
   public readonly templatesLocation: string = `${__dirname}/templates`
 }
