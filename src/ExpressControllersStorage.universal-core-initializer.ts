698bc1ff6b5aad765c31e78f737528726f4d0d91
David De Anda <omarandstuff@gmail.com>
2024-07-11 15:06:57 -0600

    Right initializer import

--- src/ExpressControllersStorage.universal-core-initializer.ts
+++ src/ExpressControllersStorage.universal-core-initializer.ts
@@ -1,4 +1,4 @@
-import CoreInitializer from '@universal-packages/core/CoreInitializer'
+import { CoreInitializer } from '@universal-packages/core'
 
 export default class ExpressControllersStorageInitializer extends CoreInitializer {
   public static readonly initializerName = 'express-controllers-storage'
